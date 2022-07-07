5ab56b4c387bdcd24b8a9a2dedfebe4b782e7e63
d3stroya <destroya@users.noreplay.github.com>
2022-07-07 18:40:59 +0200

    Initial commit

---
+++ src/main/java/com/d3stroya/restdatajpa2/RestDatajpa2Application.java
@@ -0,0 +1,13 @@
+package com.d3stroya.restdatajpa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDatajpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDatajpa2Application.class, args);
+	}
+
+}
